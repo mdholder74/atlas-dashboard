a45b3453bc4f269115a40751695c2821db8dd760
mdholder74 <mdholder74@gmail.com>
2025-08-19 20:57:00 -0400

    Add initial application module, routing, and site navigation components

---
+++ src/app/app-module.ts
@@ -0,0 +1,44 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { CouresList } from './features/coures-list/coures-list';
+import { CourseCard } from './features/course-card/course-card';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { CourseDetail } from './features/course-detail/course-detail';
+import { SiteNavHeader } from './site-nav-header/site-nav-header';
+import { CommonModule } from '@angular/common';
+import { CommitmentForm } from './features/commitment-form/commitment-form';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CodeSnippet } from './features/code-snippet/code-snippet';
+
+
+
+@NgModule({
+  declarations: [
+    App,
+    CouresList,
+    CourseCard,
+    CourseDetail,
+    SiteNavHeader,
+    CommitmentForm,
+    CodeSnippet,
+
+
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CommonModule,
+    ReactiveFormsModule
+
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideHttpClient(),
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
---
+++ src/app/app-routing-module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CourseDetail } from './features/course-detail/course-detail';
+import { CouresList } from './features/coures-list/coures-list';
+import { CommitmentForm } from './features/commitment-form/commitment-form';
+
+// Define the routes for the application
+// Each route maps a path to a component and can include a title for the page
+const routes: Routes = [
+  { path: '', redirectTo: '/courses', pathMatch: 'full' }, // Redirect empty path to courses
+  { path: 'courses', component: CouresList, title: 'Course List' },
+  { path: 'courses/:id', component: CourseDetail },
+  {
+    path: 'commit-forms/:id',
+    component: CommitmentForm,
+    title: 'Commit to Course',
+  }, // This route is for committing to a course, it uses the same component as course detail
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)], // Use forRoot in the main app routing module meaning this is the main routing configuration
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
