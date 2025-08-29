2c4814768b5079bab145f7eee02b7e766c905203
mdholder74 <mdholder74@gmail.com>
2025-08-29 09:23:09 -0400

    Update docs with correct build files

--- courses-backend/server.js
+++ courses-backend/server.js
@@ -166,7 +166,7 @@ app.get("/azureStages", (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -24,6 +24,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, intern-atlas');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, atlas-dashboard');
   });
 });
